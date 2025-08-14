4b39b89b505557d18dbb9dba4127e3332272eae6
Miguel <miguelsantosartur15@gmail.com>
2025-08-14 12:09:10 -0300

    Atualiza o layout e conteúdo do portfólio, incluindo melhorias de estilo e novas seções, além de ajustes na funcionalidade do script.

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // Seleciona o botão e o parágrafo pelo ID
     const botao = document.getElementById('meuBotao');
     const paragrafo = document.getElementById('meuParagrafo');
-
-    // Adiciona evento de clique ao botão
-    botao.addEventListener('click', function() {
-        paragrafo.textContent = 'Você clicou no botão!';
-    });
+    const musicaInput = document.getElementById('musicaInput');
+    const musica = new Audio('relaxed-scene-made-with-Voicemode.mp3');
+    musica.loop = true;
+    musica.play(); 
 });
