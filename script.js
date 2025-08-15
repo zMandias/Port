edf1be3d2d5ba99a0acecf1632ba3c892865de09
Miguel Artur <miguelsantosartur15@gmail.com>
2025-08-15 18:16:36 -0300

    Layout Changes - 2024-06-19

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 // Espera o DOM carregar
 document.addEventListener('DOMContentLoaded', function() {
-    // Seleciona o botão e o parágrafo pelo ID
     const botao = document.getElementById('meuBotao');
     const paragrafo = document.getElementById('meuParagrafo');
-    const musicaInput = document.getElementById('musicaInput');
-    const musica = new Audio('relaxed-scene-made-with-Voicemode.mp3');
-    musica.loop = true;
-    musica.play(); 
 });
